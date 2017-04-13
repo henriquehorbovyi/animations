4282a4d1851302551929f723c83df5fe3852780a
henrywm <carloshenry.wm@gmail.com>
2017-04-13 17:59:21 -0300

    Added a menu to the animations

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,35 @@
 gulp        = require("gulp");
+watch       = require("gulp-watch");
 uglify      = require("gulp-uglify");
 minifyhtml  = require("gulp-minify-html");
 minifycss   = require("gulp-minify-css");
 
+paths = {
+  js: "src/scripts/*.js",
+  css: "src/css/*.css",
+  html: "src/view/*.html",
+};
 
-gulp.task("default",function(){
-  console.log("gulp it's ok");
+gulp.task("default",['compress-all']);
+gulp.task("watch",function(){
+    gulp.watch(paths.js,["compress-js"]);
+    gulp.watch(paths.html,["compress-html"]);
+    gulp.watch(paths.css,["compress-css"]);
 });
-
 gulp.task("compress-all", ["compress-js","compress-html","compress-css"]);
 
 gulp.task("compress-js",function(){
-  gulp.src("src/scripts/*.js")
+  gulp.src(paths.js)
   .pipe(uglify())
   .pipe(gulp.dest("dist/js/"));
 });
 gulp.task("compress-html",function(){
-  gulp.src("src/view/*.html")
+  gulp.src(paths.html)
   .pipe(minifyhtml())
   .pipe(gulp.dest("dist/view/"));
 });
 gulp.task("compress-css",function(){
-  gulp.src("src/css/*.css")
+  gulp.src(paths.css)
   .pipe(minifycss())
   .pipe(gulp.dest("dist/css/"));
 });
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ express = require("express");
 path    = require("path");
 app     = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('dist'));
 
 app.get("/", function(request, response){
-    res.sendFile(path.join(__dirname, 'index.html'))
+    response.sendFile(path.join(__dirname+"/dist/view/index.html"));
+});
+app.get("/flat-card", function(request, response){
+    response.sendFile(path.join(__dirname+"/dist/view/flat-card.html"));
 });
 
 app.listen('7000',function(){
